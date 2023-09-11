0ee1c3a7d1fcd39a887f7072241592aa53e95ec0
PatrykkM <patryk.mrzyglod3@gmail.com>
2023-09-11 20:23:08 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+window.onload = function () {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+};
