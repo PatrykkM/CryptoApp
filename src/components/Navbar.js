63c0a3620cc5937ba76c47ba8114091e2405f246
PatrykkM <patryk.mrzyglod3@gmail.com>
2023-09-12 00:32:54 +0200

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaCoins} from 'react-icons/fa'
+import {FaSearchDollar} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <Link to='/'>
             <div className='navbar'>
-                <FaCoins className='icon' />
+                <FaSearchDollar className='icon' />
                 <h1> Coin <span className='purple'>Search</span></h1>
             </div>
         </Link>
